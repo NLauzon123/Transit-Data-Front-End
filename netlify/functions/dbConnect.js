d94550b6482162b6a295351aafeb3e07e4dee52b
Kevin Britten <kevinjbritten@gmail.com>
2024-08-15 16:35:53 -0400

    update serverless function to accept params and procedure name from front-end

--- netlify/functions/dbConnect.js
+++ netlify/functions/dbConnect.js
@@ -12,41 +12,43 @@ const config = {
   },
 };
 
-console.log("DB Config:", config); // Log the DB configuration
-
-// const queries = {
-//   top10fare: `SELECT TOP (10) [fareID]
-//       ,[fareUserID]
-//       ,[fareType]
-//       ,[startDate]
-//       ,[endDate]
-//       ,[quantityRemaining]
-//   FROM [Transit].[dbo].[Fare]`,
-// };
+const allowedProcedures = ["MidTermTraffic", "AnotherProcedure"];
 
 exports.handler = async function (event, context) {
-  // const queryName = event.queryStringParameters.queryName;
-  // const query = queries[queryName];
+  // Parse the request body
+  const requestData = JSON.parse(event.body);
 
-  // console.log("Received query:", queryName);
-  // console.log("SQL Query:", query);
+  // Check if the procedure is allowed
+  if (!allowedProcedures.includes(requestData.procedure)) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "Invalid procedure name." }),
+    };
+  }
 
-  // if (!query) {
-  //   return {
-  //     statusCode: 400,
-  //     body: JSON.stringify({ error: `Query "${queryName}" not found.` }),
-  //   };
-  // }
+  // Validate the params before proceeding
+  const isValid = requestData.params.every((param) => {
+    return (
+      typeof param.key === "string" &&
+      typeof param.value !== "undefined" &&
+      sql[param.type] // Ensure the type is valid in mssql
+    );
+  });
+
+  if (!isValid) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: "Invalid parameters." }),
+    };
+  }
+
   try {
     console.log("Attempting to connect to the database...");
     let pool = await sql.connect(config);
     console.log("Connected to the database successfully.");
 
-    let result = await pool
-      .request()
-      .input("year", sql.Int, 2025)
-      .input("service", sql.Int, 13)
-      .execute("MidTermTraffic");
+    let request = pool.request();
+    let result = await getResult(requestData, request);
 
     console.log("Query executed successfully:", result);
     return {
@@ -57,7 +59,16 @@ exports.handler = async function (event, context) {
     console.error("SQL error", err);
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: "Server Error", details: err.message }),
+      body: JSON.stringify({ error: "Server Error" }),
     };
+  }
+
+  async function getResult(requestData, request) {
+    let { procedure, params } = requestData;
+    params.forEach((param) => {
+      let { key, type, value } = param;
+      request.input(key, sql[type], value);
+    });
+    return await request.execute(procedure);
   }
 };
