e7acb0e11f5f4113e5f30096c229bf2c8205200e
Choo-Laptop\Choo <choohuiser@yahoo.com>
2024-08-14 18:16:29 -0900

    Small update on validation , not done yet

---
+++ js/settings.js
@@ -0,0 +1,6 @@
+"use strict"
+//First check the old password match the one on DB
+// check null or empty input
+//If new password does not meet the requirement : "Must contain at least one number, 
+//              one uppercase and lowercase letter, and at least 8 characters"
+//If new password != reconfirm password -> ""
--- js/signup.js
+++ js/signup.js
@@ -1,5 +1,78 @@
 'use strict';
 
+//On change to detect changes on inputs
+function validateAgencyID() {
+  var agencyIDInput = document.getElementById('inputAgencyID').value;
+  var message = document.getElementById('error__agencyID');
+
+  if (agencyIDInput !== ' ' && agencyIDInput.length > 0) {
+    message.textContent = 'Checked';
+    message.style.color = 'green';
+  } else {
+    agencyIDInput.clear();
+    message.textContent = 'Please enter your angency ID 22';
+    message.style.color = 'red';
+  }
+}
+
+function validateFName() {
+  var fNameInput = document.getElementById('inputFName').value;
+  var message = document.getElementById('error__fname');
+
+  if (fNameInput !== ' ' && fNameInput.length > 0) {
+    message.textContent = 'Checked';
+    message.style.color = 'green';
+  } else {
+    fNameInput = "";
+    message.textContent = 'Please key in your first name 22';
+    message.style.color = 'red';
+  }
+}
+
+function validateLName() {
+  var lNameInput = document.getElementById('inputLName').value;
+  var message = document.getElementById('error__lname');
+
+  if (lNameInput !== ' ' && lNameInput.length > 0) {
+    message.textContent = 'checked';
+    message.style.color = 'green';
+  } else {
+    lNameInput = "";
+    message.textContent = 'Please key in your last name 22';
+    message.style.color = 'red';
+  }
+}
+
+function validateUsername() {
+  var usernameInput = document.getElementById('inputUsername').value;
+  var message = document.getElementById('error__username');
+
+  if (usernameInput !== ' ' && usernameInput.length > 0) {
+    message.textContent = 'checked';
+    message.style.color = 'green';
+  } else {
+    usernameInput = "";
+    message.textContent = 'Please key in your username 22';
+    message.style.color = 'red';
+  }
+}
+
+function validatePassword() {
+  var passwordInput = document.getElementById('inputPassword').value;
+  var message = document.getElementById('error__password');
+
+  // Simple password validation pattern
+  var regexPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&^])[A-Za-z\d@.#$!%*?&]{8,15}$/;
+  if (passwordInput.match(regexPattern)) {
+    message.textContent = 'checked';
+    message.style.color = 'green';
+  } else {
+    passwordInput = "";
+    message.textContent = 'Your password should have minimum length of 8,  with at least a symbol, upper and lower case letters and a number';
+    message.style.color = 'red';
+  }
+}
+
 let formAjaxValidate = document.getElementById('form_ajax_validate');
 let checkForm = function (e) {
   let form = e.target;
