1fc8cabfcd58a8d098b2679aaef36c28a83f0551
Kevin Britten <kevinjbritten@gmail.com>
2024-08-19 19:40:58 -0400

    add loginUser and checkToken to apicalls and test implimentations to test.html

--- js/apiCalls.js
+++ js/apiCalls.js
@@ -17,15 +17,75 @@ const getDataFromProcedure = async (requestData) => {
       // Parse the JSON data from the response
       const data = await response.json();
       // return the data
-      return JSON.stringify(data);
+      return data;
     } else {
       // Handle errors (e.g., non-200 status codes)
       return { error: `Error: ${response.statusText}` };
     }
   } catch (error) {
     // Handle network errors or other issues
-    document.getElementById("result").innerText = `Error: ${error.message}`;
+    console.log(`Error: ${error.message}`);
   }
 };
 
-export { getDataFromProcedure };
+const loginUser = async (requestData) => {
+  try {
+    // Make the request to the Netlify function
+    const response = await fetch(
+      `https://transitvanierbackend.azurewebsites.net/api/loginUser`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestData),
+      }
+    );
+
+    // Check if the response is OK (status code 200)
+    if (response.ok) {
+      // Parse the JSON data from the response
+      const data = await response.json();
+      // return the data
+      return data;
+    } else {
+      // Handle errors (e.g., non-200 status codes)
+      return { error: `Error: ${response.statusText}` };
+    }
+  } catch (error) {
+    // Handle network errors or other issues
+    console.log(`Error: ${error.message}`);
+  }
+};
+
+const checkToken = async (requestData) => {
+  try {
+    // Make the request to the Netlify function
+    const response = await fetch(
+      `https://transitvanierbackend.azurewebsites.net/api/checkToken`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestData),
+      }
+    );
+
+    // Check if the response is OK (status code 200)
+    if (response.ok) {
+      // Parse the JSON data from the response
+      const data = await response.json();
+      // return the data
+      return data;
+    } else {
+      // Handle errors (e.g., non-200 status codes)
+      return { error: `Error: ${response.statusText}` };
+    }
+  } catch (error) {
+    // Handle network errors or other issues
+    console.log(`Error: ${error.message}`);
+  }
+};
+
+export { getDataFromProcedure, loginUser, checkToken };
