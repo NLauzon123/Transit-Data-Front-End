94f5fc35f55fa408c4cb7118487d34776aa510d8
Kevin Britten <kevinjbritten@gmail.com>
2024-08-21 15:28:17 -0400

    add logout and change password functions to apiCalls.js

--- js/apiCalls.js
+++ js/apiCalls.js
@@ -2,7 +2,7 @@ const getDataFromProcedure = async (requestData) => {
   try {
     // Make the request to the Netlify function
     const response = await fetch(
-      `https://transitvanierbackend.azurewebsites.net/api/getDataFromProcedure`,
+      `https://transitvanierbackend.azurewebsites.net/api/getdatafromprocedure`,
       {
         method: "POST",
         credentials: "include",
@@ -33,7 +33,7 @@ const signupUser = async (requestData) => {
   try {
     // Make the request to the Netlify function
     const response = await fetch(
-      `https://transitvanierbackend.azurewebsites.net/api/signupUser`,
+      `https://transitvanierbackend.azurewebsites.net/api/signupuser`,
       {
         method: "POST",
         credentials: "include",
@@ -64,7 +64,7 @@ const loginUser = async (requestData) => {
   try {
     // Make the request to the Netlify function
     const response = await fetch(
-      `https://transitvanierbackend.azurewebsites.net/api/loginUser`,
+      `https://transitvanierbackend.azurewebsites.net/api/loginuser`,
       {
         method: "POST",
         credentials: "include",
@@ -91,11 +91,11 @@ const loginUser = async (requestData) => {
   }
 };
 
-const checkToken = async () => {
+const logoutUser = async (requestData) => {
   try {
     // Make the request to the Netlify function
     const response = await fetch(
-      `https://transitvanierbackend.azurewebsites.net/api/checkToken`,
+      `https://transitvanierbackend.azurewebsites.net/api/logoutuser`,
       {
         method: "POST",
         credentials: "include",
@@ -121,4 +121,72 @@ const checkToken = async () => {
   }
 };
 
-export { getDataFromProcedure, signupUser, loginUser, checkToken };
+const checkToken = async () => {
+  try {
+    // Make the request to the Netlify function
+    const response = await fetch(
+      `https://transitvanierbackend.azurewebsites.net/api/checktoken`,
+      {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    // Check if the response is OK (status code 200)
+    if (response.ok) {
+      // Parse the JSON data from the response
+      const data = await response.json();
+      // return the data
+      return data;
+    } else {
+      // Handle errors (e.g., non-200 status codes)
+      return { error: `Error: ${response.statusText}` };
+    }
+  } catch (error) {
+    // Handle network errors or other issues
+    console.log(`Error: ${error.message}`);
+  }
+};
+
+const changePassword = async (requestData) => {
+  try {
+    // Make the request to the Netlify function
+    const response = await fetch(
+      `https://transitvanierbackend.azurewebsites.net/api/changepassword`,
+      {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestData),
+      }
+    );
+
+    // Check if the response is OK (status code 200)
+    if (response.ok) {
+      // Parse the JSON data from the response
+      const data = await response.json();
+      // return the data
+      return data;
+    } else {
+      // Handle errors (e.g., non-200 status codes)
+      return { error: `Error: ${response.statusText}` };
+    }
+  } catch (error) {
+    // Handle network errors or other issues
+    console.log(`Error: ${error.message}`);
+  }
+};
+
+export {
+  getDataFromProcedure,
+  signupUser,
+  loginUser,
+  logoutUser,
+  checkToken,
+  changePassword,
+};
